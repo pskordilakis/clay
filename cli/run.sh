df80d24e3e680b0fcf3da0aef22b2bd49970b0e2
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2019-01-05 17:00:55 +0200

    get input and output dump files from arguments

--- cli/run.sh
+++ cli/run.sh
@@ -3,12 +3,17 @@
 # Run migrations against the database
 
 function run {
+    if [[ ! -f $1 ]]; then
+        printf "%s\n" "File $1 does not exist" 1>&2
+        exit 1
+    fi
+
     # Import database
-    docker-compose exec -T mysql mysql -uroot -ptopsecret clay < ./db/dump.sql
+    docker-compose exec -T mysql mysql -uroot -ptopsecret clay < $1
 
     # Run
     docker-compose run node node ./dist/clay.cjs.js
 
     # export new database
-    docker-compose exec mysql mysqldump -uroot -ptopsecret --no-create-info clay > mydb.sql
+    docker-compose exec mysql mysqldump -uroot -ptopsecret --no-create-info clay > $2
 }
